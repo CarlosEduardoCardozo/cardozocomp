9ac3421a9b01fc3de7035bfd48f66b439759676e
GCFilmes <cardozogumsc@gmail.com>
2022-04-27 16:24:15 -0300

    first commit

---
+++ src/components/MenuCabecalho.vue
@@ -0,0 +1,7 @@
+<script></script>
+<template>
+  <header id="header">
+    <button id="botao" class="BtnLogin">Entrar</button>
+    <h1 id="cabecalho" class="nav">Forget Store</h1>
+  </header>
+</template>
---
+++ src/components/MenuRodape.vue
@@ -0,0 +1,6 @@
+<script></script>
+<template>
+  <footer id="footer" class="mens">Um conceito diferente o original forget</footer>
+  <p id="footer">Mande uma mensagem para mais informações</p>
+
+</template>
